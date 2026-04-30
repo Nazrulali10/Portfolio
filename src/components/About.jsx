8e961a41148c2f1b1fcae1830fa5d05504898c2d
Nazrul Ali.N <nazrulali555@gmail.com>
2026-04-30 16:07:22 +0530

    animation

--- src/components/About.jsx
+++ src/components/About.jsx
@@ -1,14 +1,19 @@
 
-
+import { motion } from 'framer-motion'
 const About = () => {
   return (
-    <div className='flex flex-col gap-2 px-4 md:px-18 py-12'>
+    <motion.div
+      initial={{ opacity: 0 }}
+      whileInView={{ opacity: 1}}
+      transition={{ duration: 2, ease: "easeOut" }}
+      viewport={{ once: true }}
+     className='flex flex-col gap-2 px-4 md:px-18 py-12'>
         <h1 className="text-3xl md:text-4xl">About</h1>
         <p className="text-lg px-6 py-2">Enthusiastic <span className='text-rose-500'>Full Stack Developer</span> with hands-on experience in building projects using
 Next.js and the MERN stack. Skilled in developing efficient, high-quality web solutions using
 both AI-powered tools and traditional development practices. Looking for an opportunity to
 grow as a developer and contribute to modern, fast-paced engineering teams.</p>
-    </div>
+    </motion.div>
   )
 }
 
--- src/components/SkillCard.jsx
+++ src/components/SkillCard.jsx
@@ -1,8 +1,13 @@
-
+import { motion } from 'framer-motion'
 
 const SkillCard = ({item}) => {
   return (
-    <div className='group flex flex-col justify-center  bg-gray-50 hover:bg-rose-400 rounded-3xl px-12 py-6 gap-2 md:gap-4 hover:scale-105 hover:text-white marker:text-rose-500 hover:marker:text-white duration-300 transition-all'>
+    <motion.div
+     initial={{ opacity: 0 }}
+      whileInView={{ opacity: 1}}
+      transition={{ duration: 1, ease: "easeOut" }}
+      viewport={{ once: true }}
+     className='group flex flex-col justify-center  bg-gray-50 hover:bg-rose-400 rounded-3xl px-12 py-6 gap-2 md:gap-4 hover:scale-105 hover:text-white marker:text-rose-500 hover:marker:text-white duration-300 transition-all'>
         <div>
              <h1 className='text-xl '>{item.category}</h1>
         </div>
@@ -13,7 +18,7 @@ const SkillCard = ({item}) => {
         ))}
         </div>
       
-    </div>
+    </motion.div>
   )
 }
 
